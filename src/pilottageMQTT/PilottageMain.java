b3722deed15687de2d585755cc400665682f5632
E238898X <E238898X@etu.univ-nantes.fr>
2024-02-03 12:01:25 +0100

    Fixed unknown host problem for access points networks, ajdusted code

---
+++ src/pilottageMQTT/PilottageMain.java
@@ -0,0 +1,25 @@
+package pilottageMQTT;
+
+import lejos.utility.Delay;
+
+public class PilottageMain {
+
+	public static void main(String[] args) throws Exception {
+		// MQTT Connexion configuration
+		final String MQTT_SERVER_IP = "192.168.127.124";
+		final String clientId = "EV3_" + Utils.generateClientID();
+		final String topic = "ev3/topic";
+
+		try {
+			new MQTTConnect(MQTT_SERVER_IP, clientId, topic);
+		} catch (InterruptedException e) {
+			Utils.print("\n\n\n" + e.getMessage());
+		} catch (Exception e) {
+			Utils.print("\n\n\n" + e.getMessage());
+		} finally {
+			Delay.msDelay(2000);
+			System.exit(0);
+		}
+	}
+
+}
