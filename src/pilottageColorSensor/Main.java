55e74004290964ac1de374d85e9a3cfc1d19841c
Mamadou cire Camara <mamadoucirecamara@gmail.com>
2024-02-22 09:41:42 +0100

    ajout conduite by file json

--- src/pilottageColorSensor/Main.java
+++ src/pilottageColorSensor/Main.java
@@ -1,14 +1,21 @@
 package pilottageColorSensor;
+import java.io.File;
 
+import lejos.hardware.Sound;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+	/*	
 		ConduiteAutonome conduite = new ConduiteAutonome();
+	
 		try {
-			conduite.test();
+		//	conduite.test();
 		} catch (InterruptedException e) {
 			System.out.println(e.getMessage());
 		} catch (RuntimeException e) {
@@ -17,6 +24,8 @@ public class Main {
 			Delay.msDelay(1500);
 		}
 		
-		System.exit(0);
+		System.exit(0);*/
+
+		   
 	}
 }
